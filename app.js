f3e8f4d8834c45fc87455a0fd2eb395eb0df5302
Veljko <veljkoBitno@gmail.com>
2023-02-10 17:58:39 +0100

    Finished js and styling

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 const questions = document.querySelectorAll(".main__question");
-const answers = document.querySelectorAll(".main__question__answer");
+
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const text = question.children[0].children[0];
+    const image = question.children[0].children[1];
+    const answer = question.children[1];
+
+    if (answer.style.display === "none" || answer.style.display === "") {
+      answer.style.display = "block";
+      text.classList.add("main__question__text--bold");
+      image.style.transform = "rotate(180deg)";
+    } else {
+      answer.style.display = "none";
+      text.classList.remove("main__question__text--bold");
+      image.style.transform = "rotate(360deg)";
+    }
+  });
+});
